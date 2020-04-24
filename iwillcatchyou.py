c0f94b78804482687186c57701f045af321ae2b9
FML <1244783871@qq.com>
2020-04-24 15:48:39 +0800

    加了加注释（大概

--- iwillcatchyou.py
+++ iwillcatchyou.py
@@ -12,18 +12,17 @@ c=b'BM'
 d=b'%PDF'
 e=b'PK\x03\x04'
 f=b'PK\x03\x04'
-g=b'MZ'  ##这样写好像简单点
-catch_dict={".jpg":a,".png":b,'.bmp':c,'.pdf':d,'.docx':e,'.pptx':f,'.exe':g} 
+g=b'MZ'  ##这样写快一点（大概
+catch_dict={".jpg":a,".png":b,'.bmp':c,'.pdf':d,'.docx':e,'.pptx':f,'.exe':g} #用来对比文件是否正常
 
 path = Path.cwd() ##获取当前路径
 
 Path(path/'异常').mkdir(exist_ok=True)
-Path(path/'正常').mkdir(exist_ok=True)
-##创建新目录在当前路径
+Path(path/'正常').mkdir(exist_ok=True)#创建新目录在当前路径
 
-#遍历文件
-for file in files:
-    if Path(file).is_file():
+
+for file in files:#遍历文件
+    if Path(file).is_file():#如果他是文件
         suffix=file.suffix #获取后缀
         with open(file, 'rb') as f:#读取文件
             file_header = f.read(len(catch_dict[suffix]))#判断正常异常
@@ -32,4 +31,4 @@ for file in files:
         else :
             file.rename(path/'异常'/(file.stem+file.suffix))
         
-print('已结束操作')
+print('呔，妖怪，哪里逃~')#嗯 没啥难度
